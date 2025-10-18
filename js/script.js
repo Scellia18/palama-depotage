46e5ef7d398c48d2c44522f09290e744cdad1d54
Scellia18 <kalyani18.araye@gmail.com>
2025-10-18 16:11:04 +0400

    Correction fichier js formulaire + infos mentions

--- js/script.js
+++ js/script.js
@@ -19,3 +19,26 @@ document.getElementById("monForm").addEventListener("submit", function(e) {
       alert("⚠️ Merci de remplir tous les champs obligatoires.");
     }
 });
+
+
+document.getElementById("monForm2").addEventListener("submit", function(e) {
+    e.preventDefault(); // empêche l’envoi automatique du formulaire
+
+    // Récupérer tous les champs obligatoires
+    let champsObligatoires = this.querySelectorAll("[required]");
+    let toutRempli = true;
+
+    champsObligatoires.forEach(champ => {
+      if (!champ.value.trim()) { // si vide
+        toutRempli = false;
+      }
+    });
+
+    if (toutRempli) {
+      alert("Votre message à bien été envoyé !");
+      // Ici, vous pouvez soumettre le formulaire si besoin :
+      this.submit();
+    } else {
+      alert("⚠️ Merci de remplir tous les champs obligatoires.");
+    }
+});
